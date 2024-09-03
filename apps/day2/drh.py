d296c5ce4fe1e5ff15f27468e1308b31971aa16b
GemmaTuron <gemma@ersilia.io>
2024-09-03 17:06:14 +0200

    wip day 2 breakout

--- apps/day2/drh.py
+++ apps/day2/drh.py
@@ -1,20 +1,34 @@
 import pandas as pd
 from rdkit import Chem
+from standardiser import standardise
 
-# Step 1: Load the data, skipping header lines that start with '!'
 file_path = "data/repurposing_samples_20240610.txt"
 df = pd.read_csv(file_path, sep='\t', comment='!')
+df_ = df[['smiles', 'InChIKey', 'pubchem_cid']]
+print(df_.shape)
 
-# Step 2: Select the columns 'smiles', 'InChIKey', and 'pubchem_cid'
-df_filtered = df[['smiles', 'InChIKey', 'pubchem_cid']]
-
-# Step 3: Save the filtered data to a CSV file
 output_path = "data/drugrepurposinghub.csv"
-df_filtered = df_filtered.drop_duplicates(keep = "first")
-def is_valid_smiles(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    return mol is not None
-df_filtered = df_filtered[df_filtered['smiles'].apply(is_valid_smiles)]
-df_filtered.to_csv(output_path, index=False)
+df_ = df_.drop_duplicates(keep = "first")
+print(df_.shape)
 
-print(f"Data saved to {output_path}")
+smiles = []
+for s in df_["smiles"]:
+    mol = Chem.MolFromSmiles(s)
+    if mol is not None:
+        try:
+            mol_ = standardise.run(mol)
+            s_ = Chem.MolToSmiles(mol_)
+        except:
+            print("Non standard:", s)
+            s_ = None
+    else:
+        print("No rdkit:" ,s)
+        s_ = None
+    smiles += [s_]
+
+df_["smiles_ok"] = smiles
+df_ok = df_[~df_["smiles_ok"].isna()]
+print(df_ok.shape)
+
+df_ok.to_csv(output_path, index=False)
+
