1edcdc7c6ae167068182e10c6ca6c95c0d7f2230
GemmaTuron <gemma@ersilia.io>
2024-09-04 11:17:00 +0200

    add aptfile for do

--- apps/day2/ersilia_models.sh
+++ apps/day2/ersilia_models.sh
@@ -1,61 +1,55 @@
 ersilia serve eos3804
-ersilia -v run -i data/drugrepurposinghub_subset_1.csv -o data/drugrepurposinghub_subset_1_eos3804.csv
-ersilia -v run -i data/drugrepurposinghub_subset_2.csv -o data/drugrepurposinghub_subset_2_eos3804.csv
-ersilia -v run -i data/drugrepurposinghub_subset_3.csv -o data/drugrepurposinghub_subset_3_eos3804.csv
-ersilia -v run -i data/drugrepurposinghub_subset_4.csv -o data/drugrepurposinghub_subset_4_eos3804.csv
-ersilia -v run -i data/drugrepurposinghub_subset_5.csv -o data/drugrepurposinghub_subset_5_eos3804.csv
+ersilia -v run -i data/drugrepurposinghub_subset1.csv -o data/drugrepurposinghub_subset1_eos3804.csv
+ersilia -v run -i data/drugrepurposinghub_subset2.csv -o data/drugrepurposinghub_subset2_eos3804.csv
+ersilia -v run -i data/drugrepurposinghub_subset3.csv -o data/drugrepurposinghub_subset3_eos3804.csv
+ersilia -v run -i data/drugrepurposinghub_subset4.csv -o data/drugrepurposinghub_subset4_eos3804.csv
+ersilia -v run -i data/drugrepurposinghub_subset5.csv -o data/drugrepurposinghub_subset5_eos3804.csv
 ersilia close
 
-ersilia -v fetch eos9ei3 --from_dockerhub
 ersilia serve eos9ei3
-ersilia -v run -i data/drugrepurposinghub_subset_1.csv -o data/drugrepurposinghub_subset_1_eos9ei3.csv
-ersilia -v run -i data/drugrepurposinghub_subset_2.csv -o data/drugrepurposinghub_subset_2_eos9ei3.csv
-ersilia -v run -i data/drugrepurposinghub_subset_3.csv -o data/drugrepurposinghub_subset_3_eos9ei3.csv
-ersilia -v run -i data/drugrepurposinghub_subset_4.csv -o data/drugrepurposinghub_subset_4_eos9ei3.csv
-ersilia -v run -i data/drugrepurposinghub_subset_5.csv -o data/drugrepurposinghub_subset_5_eos9ei3.csv
+ersilia -v run -i data/drugrepurposinghub_subset1.csv -o data/drugrepurposinghub_subset1_eos9ei3.csv
+ersilia -v run -i data/drugrepurposinghub_subset2.csv -o data/drugrepurposinghub_subset2_eos9ei3.csv
+ersilia -v run -i data/drugrepurposinghub_subset3.csv -o data/drugrepurposinghub_subset3_eos9ei3.csv
+ersilia -v run -i data/drugrepurposinghub_subset4.csv -o data/drugrepurposinghub_subset4_eos9ei3.csv
+ersilia -v run -i data/drugrepurposinghub_subset5.csv -o data/drugrepurposinghub_subset5_eos9ei3.csv
 ersilia close
 
-ersilia -v fetch eos2ta5 --from_dockerhub
 ersilia serve eos2ta5
-ersilia -v run -i data/drugrepurposinghub_subset_1.csv -o data/drugrepurposinghub_subset_1_eos2ta5.csv
-ersilia -v run -i data/drugrepurposinghub_subset_2.csv -o data/drugrepurposinghub_subset_2_eos2ta5.csv
-ersilia -v run -i data/drugrepurposinghub_subset_3.csv -o data/drugrepurposinghub_subset_3_eos2ta5.csv
-ersilia -v run -i data/drugrepurposinghub_subset_4.csv -o data/drugrepurposinghub_subset_4_eos2ta5.csv
-ersilia -v run -i data/drugrepurposinghub_subset_5.csv -o data/drugrepurposinghub_subset_5_eos2ta5.csv
+ersilia -v run -i data/drugrepurposinghub_subset1.csv -o data/drugrepurposinghub_subset1_eos2ta5.csv
+ersilia -v run -i data/drugrepurposinghub_subset2.csv -o data/drugrepurposinghub_subset2_eos2ta5.csv
+ersilia -v run -i data/drugrepurposinghub_subset3.csv -o data/drugrepurposinghub_subset3_eos2ta5.csv
+ersilia -v run -i data/drugrepurposinghub_subset4.csv -o data/drugrepurposinghub_subset4_eos2ta5.csv
+ersilia -v run -i data/drugrepurposinghub_subset5.csv -o data/drugrepurposinghub_subset5_eos2ta5.csv
 ersilia close
 
-ersilia -v fetch eos43at --from_dockerhub
 ersilia serve eos43at
-ersilia -v run -i data/drugrepurposinghub_subset_1.csv -o data/drugrepurposinghub_subset_1_eos43at.csv
-ersilia -v run -i data/drugrepurposinghub_subset_2.csv -o data/drugrepurposinghub_subset_2_eos43at.csv
-ersilia -v run -i data/drugrepurposinghub_subset_3.csv -o data/drugrepurposinghub_subset_3_eos43at.csv
-ersilia -v run -i data/drugrepurposinghub_subset_4.csv -o data/drugrepurposinghub_subset_4_eos43at.csv
-ersilia -v run -i data/drugrepurposinghub_subset_5.csv -o data/drugrepurposinghub_subset_5_eos43at.csv
+ersilia -v run -i data/drugrepurposinghub_subset1.csv -o data/drugrepurposinghub_subset1_eos43at.csv
+ersilia -v run -i data/drugrepurposinghub_subset2.csv -o data/drugrepurposinghub_subset2_eos43at.csv
+ersilia -v run -i data/drugrepurposinghub_subset3.csv -o data/drugrepurposinghub_subset3_eos43at.csv
+ersilia -v run -i data/drugrepurposinghub_subset4.csv -o data/drugrepurposinghub_subset4_eos43at.csv
+ersilia -v run -i data/drugrepurposinghub_subset5.csv -o data/drugrepurposinghub_subset5_eos43at.csv
 ersilia close
 
-ersilia -v fetch eos4e40 --from_dockerhub
 ersilia serve eos4e41
-ersilia -v run -i data/drugrepurposinghub_subset_1.csv -o data/drugrepurposinghub_subset_1_eos4e40.csv
-ersilia -v run -i data/drugrepurposinghub_subset_2.csv -o data/drugrepurposinghub_subset_2_eos4e40.csv
-ersilia -v run -i data/drugrepurposinghub_subset_3.csv -o data/drugrepurposinghub_subset_3_eos4e40.csv
-ersilia -v run -i data/drugrepurposinghub_subset_4.csv -o data/drugrepurposinghub_subset_4_eos4e40.csv
-ersilia -v run -i data/drugrepurposinghub_subset_5.csv -o data/drugrepurposinghub_subset_5_eos4e40.csv
+ersilia -v run -i data/drugrepurposinghub_subset1.csv -o data/drugrepurposinghub_subset1_eos4e40.csv
+ersilia -v run -i data/drugrepurposinghub_subset2.csv -o data/drugrepurposinghub_subset2_eos4e40.csv
+ersilia -v run -i data/drugrepurposinghub_subset3.csv -o data/drugrepurposinghub_subset3_eos4e40.csv
+ersilia -v run -i data/drugrepurposinghub_subset4.csv -o data/drugrepurposinghub_subset4_eos4e40.csv
+ersilia -v run -i data/drugrepurposinghub_subset5.csv -o data/drugrepurposinghub_subset5_eos4e40.csv
 ersilia close
 
-ersilia -v fetch eos7d58 --from_dockerhub
 ersilia serve eos7d58
-ersilia -v run -i data/drugrepurposinghub_subset_1.csv -o data/drugrepurposinghub_subset_1_eos7d58.csv
-ersilia -v run -i data/drugrepurposinghub_subset_2.csv -o data/drugrepurposinghub_subset_2_eos7d58.csv
-ersilia -v run -i data/drugrepurposinghub_subset_3.csv -o data/drugrepurposinghub_subset_3_eos7d58.csv
-ersilia -v run -i data/drugrepurposinghub_subset_4.csv -o data/drugrepurposinghub_subset_4_eos7d58.csv
-ersilia -v run -i data/drugrepurposinghub_subset_5.csv -o data/drugrepurposinghub_subset_5_eos7d58.csv
+ersilia -v run -i data/drugrepurposinghub_subset1.csv -o data/drugrepurposinghub_subset1_eos7d58.csv
+ersilia -v run -i data/drugrepurposinghub_subset2.csv -o data/drugrepurposinghub_subset2_eos7d58.csv
+ersilia -v run -i data/drugrepurposinghub_subset3.csv -o data/drugrepurposinghub_subset3_eos7d58.csv
+ersilia -v run -i data/drugrepurposinghub_subset4.csv -o data/drugrepurposinghub_subset4_eos7d58.csv
+ersilia -v run -i data/drugrepurposinghub_subset5.csv -o data/drugrepurposinghub_subset5_eos7d58.csv
 ersilia close
 
-ersilia -v fetch eos9yui --from_dockerhub
 ersilia serve eos9yui
-ersilia -v run -i data/drugrepurposinghub_subset_1.csv -o data/drugrepurposinghub_subset_1_eos9yui.csv
-ersilia -v run -i data/drugrepurposinghub_subset_2.csv -o data/drugrepurposinghub_subset_2_eos9yui.csv
-ersilia -v run -i data/drugrepurposinghub_subset_3.csv -o data/drugrepurposinghub_subset_3_eos9yui.csv
-ersilia -v run -i data/drugrepurposinghub_subset_4.csv -o data/drugrepurposinghub_subset_4_eos9yui.csv
-ersilia -v run -i data/drugrepurposinghub_subset_5.csv -o data/drugrepurposinghub_subset_5_eos9yui.csv
+ersilia -v run -i data/drugrepurposinghub_subset1.csv -o data/drugrepurposinghub_subset1_eos9yui.csv
+ersilia -v run -i data/drugrepurposinghub_subset2.csv -o data/drugrepurposinghub_subset2_eos9yui.csv
+ersilia -v run -i data/drugrepurposinghub_subset3.csv -o data/drugrepurposinghub_subset3_eos9yui.csv
+ersilia -v run -i data/drugrepurposinghub_subset4.csv -o data/drugrepurposinghub_subset4_eos9yui.csv
+ersilia -v run -i data/drugrepurposinghub_subset5.csv -o data/drugrepurposinghub_subset5_eos9yui.csv
 ersilia close
