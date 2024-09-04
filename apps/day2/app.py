c39fa262563d9066b2295acf2cb9582902b794fc
GemmaTuron <gemma@ersilia.io>
2024-09-04 14:09:16 +0200

    day 2 cache data

--- apps/day2/app.py
+++ apps/day2/app.py
@@ -97,35 +97,9 @@ if st.session_state['step1_button']:
         st.divider()
         st.header("Step 3: Featurise molecules")
 
-        @st.cache_data
-        def sample_model_urls():
-            random.seed(st.session_state.random_seed)
-            model_urls = {
-                "eos4tw0": random.choice(model_urls_list["eos4tw0"]),
-                "eos4u6p": random.choice(model_urls_list["eos4u6p"])
-            }
-            return model_urls
-
-        model_urls = sample_model_urls()
-
-        @st.cache_resource
-        def get_client(model_id):
-            return ErsiliaClient(model_urls[model_id])
-        clients = {model_id: get_client(model_id) for model_id in model_urls.keys()}
-
         @st.cache_data(show_spinner=False)
-        def run_predictive_models(model_ids, smiles_list):
-            results = {}
-            for  model_id in model_ids:
-                client = clients[model_id]
-                result = client.run(smiles_list)
-                results[model_id] = result
-            df = pd.DataFrame({"SMILES": smiles_list})
-            for model_id, result in results.items():
-                columns = list(result.columns)
-                columns = [column for column in columns if column != "input"]
-                df = pd.concat([df, result[columns]], axis=1)
-            return df
+        def load_dataframe(url):
+            return pd.read_csv(url)
     
         @st.cache_data(show_spinner=False)
         def do_plot_lolp(X, y):
@@ -150,7 +124,7 @@ if st.session_state['step1_button']:
             else:
                 with st.spinner("Running Ersilia model..."):
                     url = "https://ai2050-workshops.s3.eu-central-1.amazonaws.com/eos4wt0_preds.csv"
-                    desc1 = pd.read_csv(url)  
+                    desc1 = load_dataframe(url)
                     st.session_state['desc1_results'] = desc1
                     X = desc1.iloc[:, 2:]
                     st.session_state["desc1_lolp"] = lolp_reducer(X, y)
@@ -174,7 +148,7 @@ if st.session_state['step1_button']:
             else:
                 with st.spinner("Running Ersilia model..."):
                     url = "https://ai2050-workshops.s3.eu-central-1.amazonaws.com/eos4u6p_preds.csv"
-                    desc2 = pd.read_csv(url)
+                    desc2 = load_dataframe(url)
                     st.session_state['desc2_results'] = desc2
                     X = desc2.iloc[:, 2:]
                     st.session_state['desc2_lolp'] = lolp_reducer(X, y)
